1cf4c3a288fc9d1a84546d20ee0f4389de051b81
Justin Carver <justin.carver@hotmail.com>
2021-12-28 01:47:58 -0600

    Promoted global keyboard event to state.

--- src/components/Thoccy.js
+++ src/components/Thoccy.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import XMLToReact from 'xml-to-react';
 import { english100 } from '../conf/layouts.js'
 import Key from './Key';
@@ -7,16 +8,19 @@ import Key from './Key';
 // ! Thoccy should not be generating <Key /> components from scratch.
 const Thoccy = () => {
 
+    const [firedEvent, setFiredEvent] = useState('');
+
     const xmlToReact = new XMLToReact({
         layout : (attrs) => ({ type: motion.div, props: {...attrs, className : "layout"}}),
         row: (attrs) => ({ type: motion.div, props: {...attrs, className : "row"}}),
         keyGroup: (attrs) => ({ type: motion.div, props: {...attrs, className : "key-group"}}),
-        key : (attrs) => ({ type: Key, props : {...attrs, className : `Key`}})
+        key : (attrs) => ({ type: Key, props : {...attrs, className : `Key`, firedEvent}})
     });
 
     const reactTree = xmlToReact.convert(english100);
 
     document.addEventListener('keydown', function(event){
+        setFiredEvent(event);
         document.getElementById(event.code).classList.remove('pressed');
         document.getElementById(event.code).classList.remove('blank');
         document.getElementById(event.code).classList.add('held');
